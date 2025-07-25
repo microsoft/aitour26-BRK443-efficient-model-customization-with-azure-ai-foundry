b90e7d9c06196629a1ecb6cf23f1f65150fcf2ba
Cedric Vidal <cedric@vidal.biz>
2025-07-25 05:39:44 +0000

    🔧 Update setup.sh to remove pip upgrade command
    Removed the pip upgrade command from setup.sh to streamline the setup process. The installation of required packages remains unchanged.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,11 +4,8 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WORKSPACE_DIR="${SCRIPT_DIR}/.."
 
-# Upgrade pip
-python3 -m pip install --upgrade pip --user
-
 # Install required packages
-python3 -m pip install --user -r "${WORKSPACE_DIR}/requirements.txt"
+pip install --user -r "${WORKSPACE_DIR}/requirements.txt"
 
 # Setup RAFT
 export RAFT_DIR="${WORKSPACE_DIR}/src/demo-raft/notebooks/.gorilla"
