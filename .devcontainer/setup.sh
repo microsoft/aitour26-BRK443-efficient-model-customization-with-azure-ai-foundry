ee3c996b4878c8985bac0f863df1dbd37b4b09df
Cedric Vidal <cedric@vidal.biz>
2025-07-25 05:29:18 +0000

    🔥 Remove unnecessary requirements installation from Dockerfile
    This commit removes the copying of requirements files and the installation command for Python packages from the Dockerfile, streamlining the container setup process.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,6 +4,12 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WORKSPACE_DIR="${SCRIPT_DIR}/.."
 
+# Upgrade pip
+python3 -m pip install --upgrade pip --user
+
+# Install required packages
+python3 -m pip install --user -r "${WORKSPACE_DIR}/requirements.txt"
+
 # Setup RAFT
 export RAFT_DIR="${WORKSPACE_DIR}/src/demo-raft/notebooks/.gorilla"
 sh "${WORKSPACE_DIR}/src/demo-raft/notebooks/setup_raft.sh"
